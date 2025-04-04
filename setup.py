b7552824ebb21ee27d5eda8d3de0d3d8f59e567e
marwashahid <marvashahid09@gmail.com>
2025-04-04 21:47:25 +0500

    python script, virtual environment and setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+name='Generative AI Project',
+version = '0.0.0',
+author= 'Marwa Shahid',
+author_email='marvashahid09@gmail.com',
+packages = find_packages(),
+install_requires = []
+)
